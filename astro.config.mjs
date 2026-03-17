63c526dd42b62df0edafd0e1909e0054b650a1d0
Thisara Wijesundera <21339656+TWijesundera@users.noreply.github.com>
2026-03-16 21:47:03 -0400

    update to latest cloudflare and astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,14 @@ import cloudflare from "@astrojs/cloudflare";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import showTailwindcssBreakpoint from "astro-show-tailwindcss-breakpoint";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://peridottherapy.com",
-  integrations: [sitemap(), showTailwindcssBreakpoint()],
+  trailingSlash: "always",
+  integrations: [sitemap()],
 
   adapter: cloudflare({
     imageService: "compile",
-    platformProxy: {
-      enabled: true,
-    },
   }),
 
   vite: {
